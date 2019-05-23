82930f9e6a8a6a9ff7e873b8ec0f0a779dfb928a
Tuan Vu <36192441+tuanvvu@users.noreply.github.com>
2019-05-23 16:36:54 +0100

    Update and rename Depositionfraction.R to DF_HydrophobicParticles.R

--- R/DF_HydrophobicParticles.R
+++ R/DF_HydrophobicParticles.R
@@ -1,6 +1,5 @@
-#####ICRP MODELS
-setwd("C:/ICRP/")
-workingDirectory<-"C:/ICRP/"
+#####ICRP MODELS FOR HYDROPHOBIC PARTICLES
+
 ###01 INPUT Diamter
 # Input data
 dth<-c(0,005,0.01,0.02,0.03,0.05,0.07,0.1,0.12,0.15,0.17,0.2,0.25,0.3,0.35,0.4,0.5,0.7,1,2,3,4,5,6,7,8,9,10,15,20)
